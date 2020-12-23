6f83856f4ee5afd2f623d7a49c3fc04432334e93
Peter McNaught <petermmcnaught@gmail.com>
2020-12-23 17:24:05 +1100

    new star component, not hookes to props yet

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -11,6 +11,7 @@
       <div class="ui label"><i class="info circle icon"></i> Details</div>
       <input type="text" readonly :value="beer.details" />
     </div>
+    <div id="app"><stars-rating :config="config"></stars-rating></div>
     <div class="actions">
       <router-link
         :to="{ name: 'edit', params: { id: this.$route.params.id } }"
@@ -23,11 +24,25 @@
 
 <script>
 import { api } from '../helpers/helpers'
+import starsRating from '../components/rating-stars'
 export default {
   name: 'show',
+  components: {
+    starsRating: starsRating,
+  },
   data() {
     return {
       beer: '',
+      config: {
+        rating: 1.0,
+        isIndicatorActive: true,
+        style: {
+          fullStarColor: '#ed8a19',
+          emptyStarColor: '#737373',
+          starWidth: 100,
+          starHeight: 100,
+        },
+      },
     }
   },
   async mounted() {
